183f481bda657f104c3ea8348d3da9f2d632390f
EUNGWANCHOI <silvergwan@gmail.com>
2026-03-12 23:33:02 +0900

    변수 문제 풀이 01

---
+++ week01/variable-gj01.js
@@ -0,0 +1,20 @@
+// admin과 name이라는 변수를 선언하세요.
+// name에 값으로 "John"을 할당해 보세요.
+// name의 값을 admin에 복사해 보세요.
+// admin의 값을 콘솔 창에 띄워보세요. "John"이 출력되어야 합니다.
+
+// 내가 푼 답
+let admin;
+let name = "John";
+admin = name;
+
+console.log(admin);
+
+// // 해답
+// let admin, name; // 변수 두 개를 동시에 선언
+
+// name = "John";
+
+// admin = name;
+
+// console.log(admin);
