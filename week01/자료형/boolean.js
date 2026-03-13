5b9d5cab4ea0efa108bd486395e58b567d757425
EUNGWANCHOI <silvergwan@gmail.com>
2026-03-13 11:43:31 +0900

    Learn about 연산자

---
+++ week01/자료형/boolean.js
@@ -0,0 +1,6 @@
+let nameFieldChecked = true; // 네임필드가 checked
+let ageFieldChecked = false; // 나이필드가 not checked
+
+let isGreater = 4 > 1;
+
+console.log(isGreater); // true, 4가 1보다 크니까.
---
+++ week01/자료형/undefined.js
@@ -0,0 +1,4 @@
+// undefined는 '값이 할당되지 않은 상태'를  나타낼 때 사용
+
+let name;
+console.log(name);
