c3804272e553858abacfdc258366132aadfbfe89
EUNGWANCHOI <silvergwan@gmail.com>
2026-03-19 10:16:03 +0900

    2차 리팩토링(헬퍼 함수로 분리하였고, 객체 분리 필요해보임)

--- week02/학습내용정리프로젝트/script.js
+++ week02/학습내용정리프로젝트/script.js
@@ -1,15 +1,29 @@
 const getPhoneFortune = (phone) => {
-  const extractNum = phone.replace(/[^0-9]/g, "");
+  const extracted = extractNum(phone); // 전화번호에서 숫자 추출
+  const splited = splitNum(extracted); // 숫자 하나씩 나누기
+  const changed = changeNum(splited); // 숫자 하나씩 숫자형 배열로 넣기
+  const sum = calcSum(changed); // 숫자 합산
+  const fortune = FORTUNES[sum]; // 운세 배열 안에서 합산이랑 똑같은 객체 찾기
+  return fortune || "유효한 전화번호를 입력해주세요"; // return
+};
 
-  const splitNum = extractNum.split("");
+const extractNum = (phone) => {
+  const extracted = phone.replace(/[^0-9]/g, "");
+  return extracted;
+};
 
-  const changeNum = splitNum.map(Number);
+const splitNum = (extracted) => {
+  const splited = extracted.split("");
+  return splited;
+};
 
-  const sum = changeNum.reduce((sum, currentNum) => sum + currentNum, 0);
+const changeNum = (splited) => {
+  const changed = splited.map(Number);
+  return changed;
+};
 
-  const fortune = FORTUNES[sum];
-
-  return fortune || "유효한 전화번호를 입력해주세요";
+const calcSum = (changed) => {
+  return changed.reduce((sum, currentNum) => sum + currentNum, 0);
 };
 
 const FORTUNES = {
